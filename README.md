a156417e70ba646f813053c9ad8a4f5ca0db2b32
NATAN ALVES DA SILVA <natan_a_silva@estudante.sesisenai.org.br>
2025-11-10 22:18:28 -0300

    a

--- README.md
+++
@@ -1,2 +0,0 @@
-# gastroTech
-Sistema de reserva de mesas, restaurantes, lanchonetes, etc.
--- inicial.js
+++
@@ -1,7 +0,0 @@
-function abrirReservar(){
-    window.location.href = "reservar.html"                                                           
-}
-
-function consultarReserva() {
-    window.location.href = "consultar.html";
-}
